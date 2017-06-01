9a7bbc25245bf7aaa71f0a3c3ea8540e9fab5309
Andras Salamon <andras.salamon@melda.info>
2017-06-01 11:26:38 +0100

    find beginning of icc

--- asp-icc-extractor.c
+++ asp-icc-extractor.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
     FILE *asp = fopen("AfterShot", "rb");
@@ -8,6 +9,13 @@ int main(int argc, char **argv) {
     rewind(asp);
     char *buffer = (char *)malloc((length+1)*sizeof(char));
     fread(buffer, length, 1, asp);
+    char *searchpos = buffer;
+    while (searchpos < buffer+length) {
+        if (strncmp(searchpos, "scnrRGB XYZ", 11)==0) {
+            printf("found\n");
+        }
+        ++searchpos;
+    }
     if (asp!=NULL) {
         fclose (asp);
     }
