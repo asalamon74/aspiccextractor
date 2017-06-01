14225049196403262057671ac998c275c47dc90c
Andras Salamon <andras.salamon@melda.info>
2017-06-01 11:11:45 +0100

    reading content of the file to a buffer

--- asp-icc-extractor.c
+++ asp-icc-extractor.c
@@ -1,7 +1,13 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
     FILE *asp = fopen("AfterShot", "rb");
+    fseek(asp, 0, SEEK_END);
+    int length = ftell(asp);
+    rewind(asp);
+    char *buffer = (char *)malloc((length+1)*sizeof(char));
+    fread(buffer, length, 1, asp);
     if (asp!=NULL) {
         fclose (asp);
     }
